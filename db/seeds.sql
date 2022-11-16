d94f8877c541cd9c3b697c070280ae15c41dd6be
Christian Luna <christianlunaduenas@gmail.com>
2022-11-16 12:51:14 -0800

    Seeding the clones

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE empires_db
+
+INSERT INTO department(name)
+VALUES ("Bad batch"), ("501"), ("Company-153")
+
+INSERT INTO role(title, salary, department_id)
+VALUES ("Captin", 70000, 1), ("Seargant", 50000, 2), ("Private", 25000, 3)
+
+INSERT INTO stormtooper(first_name, last_name, role_id, manager_id)
+VALUES ("Hunter", "Clone", 1, null), ("Hound", "Clone", 2, 1), ("Axe", "Clone", 3, 2)
